ce276acb2d157528371a0d76202f3f89b7048c39
PrabhneetKaur <prabhneet1657.be20@chitkara.edu.in>
2022-12-01 17:48:45 +0530

    commit done

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -2,8 +2,55 @@ import React from "react";
 
 export const Blog = () => {
   return (
-    <div>
-      <h1>Blog</h1>
+    <><div class="header">
+    <h2>INSIGHTS</h2>
+  </div>
+  
+  <div class="row">
+    <div class="leftcolumn">
+      <div class="card">
+        <h2>AIIMS Ransomware attack</h2>
+        <h5>29th Nov 2022</h5>
+      </div>
+      <div class="card">
+        <h2>NITI Aayog on Decarbonizing Industrial Emissions</h2>
+        <h5>29th Nov 2022</h5>
+      </div>
+      <div class="card">
+        <h2>Nirogi Haryana Health Scheme</h2>
+          <h5>29th Nov 2022</h5>
+      </div>
+      <div class="card">
+        <h2>UNDP Assistance for Workers in Waste Segregation Industry</h2>
+          <h5>30th Nov 2022</h5>
+      </div>
     </div>
+
+
+    <div class="rightcolumn">
+      <div class="card">
+        <h2>About Me</h2>
+<p>
+  Prabhneet 2010991657
+</p>
+      </div>
+      <div class="card">
+        <h3>Popular Post</h3>
+        <div class="fakeimg">Image</div>
+        <div class="fakeimg">Image</div>
+        <div class="fakeimg">Image</div>
+      </div>
+      <div class="card">
+        <h3>Follow Me</h3>
+        <p>Twitter Facebook Instagram Quora</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="footer">
+    <h2>&copy; rights reserved</h2>
+  </div>
+    </>
+    
   );
 };
--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -4,6 +4,22 @@ export const Contact = () => {
   return (
     <div>
       <h1>Contact Us</h1>
+ <div id="meet">   
+<p>
+      <label>Title  : <input type="text" /></label>
+    </p>
+    <p>
+      <label>Author : <input type="text" /></label>
+    </p>
+    <p>
+      <label>Content : <input type="text" /></label>
+    </p>
+    
+
+<p>
+       <button type="submit">Submit</button>
+    </p>
+    </div >
     </div>
   );
 };
